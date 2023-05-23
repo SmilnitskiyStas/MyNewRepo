a865d91a9f53b2c1cefaabcceb0ea88f4ec476d9
SmilnitskiyStas <stassmilnitskiy@gmail.com>
2023-05-23 22:29:01 +0300

    Add new project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input number for create array");
+            int lengthArray = int.Parse(Console.ReadLine());
+
+            int[] intArray = new int[lengthArray];
+
+            intArray = GetRandomNumbers(intArray);
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+        }
+
+        private static int[] GetRandomNumbers(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(10, 500);
+            }
+
+            return array;
+        }
+    }
+}
